7f0da3853a4a8a6e33e21348de73a058e9fa64eb
Anand Pattanashetti <134671487+AnandPattanashetti@users.noreply.github.com>
2024-05-21 09:47:40 +0530

    Update provider.tf

--- Jenkins-Ec2/provider.tf
+++ Jenkins-Ec2/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.37.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+    region = "ap-northeast-1"
+  # Configuration options
 }
